6a5582a16a372d65d3bdb847d2f2b7f631a510a4
JoKhong <JoKhong@users.noreply.github.com>
2025-11-04 11:06:15 +0800

    Display mock data

--- CURD_Practice/Controllers/PersonsController.cs
+++ CURD_Practice/Controllers/PersonsController.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using ServiceContracts.DTO;
 
 namespace CURD_Practice.Controllers
 {
     public class PersonsController : Controller
     {
+        private readonly IPersonsServices _personsServices;
+
+        public PersonsController(IPersonsServices personsServices)
+        {
+            _personsServices = personsServices;
+        }
+
         [Route("persons/index")]
         [Route("/")]
         public IActionResult Index()
         {
-            return View();
+            List<PersonResponse> allPersons = _personsServices.GetAllPersons();
+
+            return View(allPersons);
         }
     }
 }

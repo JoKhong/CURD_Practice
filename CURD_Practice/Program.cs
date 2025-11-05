2056b0938a13f3c906b49fd78e052c440a402391
JoKhong <JoKhong@users.noreply.github.com>
2025-11-05 10:07:41 +0800

    Seeding Data for DB

--- CURD_Practice/Program.cs
+++ CURD_Practice/Program.cs
@@ -1,19 +1,32 @@
-using Services;
+using Entities;
+using Microsoft.EntityFrameworkCore;
 using ServiceContracts;
+using Services;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 
 //Optional, For Dependency Injection
-builder.Services.AddSingleton<ICountriesService>( provider =>
+builder.Services.AddSingleton<ICountriesService>( 
+provider =>
 {
     return new CountryServices(true);
 });
 
-builder.Services.AddSingleton<IPersonsServices>( provider =>
+builder.Services.AddSingleton<IPersonsServices>( 
+provider =>
 {
     return new PersonServices(true);
+});
+
+builder.Services.AddDbContext<PersonsDbContext>(
+options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
 
 //Optional, For Options
--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Entities
 {
     public class Person
     {
+        [Key]
         public Guid PersonID { get; set; }
+
+        [StringLength(40)]
         public string? PersonName { get; set; }
+
+        [StringLength(40)]
         public string? Email { get; set; }
+
         public DateTime? DateOfBirth { get; set; }
+
+        [StringLength(10)]
         public string? Gender { get; set; }
+
+        //Unique ID
         public Guid? CountryId { get; set; }
+
+        [StringLength(200)]
         public string? Address { get; set; }
+
+        //bit
         public bool ReceiveNewsLetters { get; set; }
     }
 }

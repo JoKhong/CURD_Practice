6f6d3ba3a556f66aa39deda19b3ccf4b251aa214
JoKhong <JoKhong@users.noreply.github.com>
2025-11-11 12:14:02 +0800

    Create interface for repository

---
+++ RepositoryContracts/ICountriesRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+
+namespace RepositoryContracts
+{
+    /// <summary>
+    /// Data access logic for managing Countires
+    /// </summary>
+    public interface ICountriesRepository
+    {
+        Task<Country> AddCountry(Country country);
+        Task<List<Country>> GetAllCountries();
+        Task<Country?> GetCountryById(Guid countryId);
+        Task<Country?> GetCountryByName(string name);
+    }
+}
---
+++ RepositoryContracts/IPersonsRepository.cs
@@ -0,0 +1,28 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryContracts
+{
+    /// <summary>
+    /// Data access layer for managing Person entity
+    /// </summary>
+    public interface IPersonsRepository
+    {
+        Task<Person> AddPerson(Person person);
+
+        Task<List<Person>> GetAllPersons();
+
+        Task<List<Person>> GetPersonById(Guid personId);
+        Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate);
+
+        Task<bool> DeletePersonsById(Guid personId);
+
+        Task<bool> UpdatePerson(Person person);
+
+    }
+}

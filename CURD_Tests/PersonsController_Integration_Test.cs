380d42758f620e32006f018dea713a3bb8209fc6
JoKhong <JoKhong@users.noreply.github.com>
2025-11-13 14:58:58 +0800

    PersonsController_Intergration testing

--- CURD_Tests/PersonsController_Integration_Test.cs
+++ CURD_Tests/PersonsController_Integration_Test.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Fizzler;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+
 namespace CURD_Tests
 {
     public class PersonsController_Integration_Test : IClassFixture<CustomWebAppFactory>
@@ -24,6 +28,16 @@ namespace CURD_Tests
 
             //Assert
             response.IsSuccessStatusCode.Should().BeTrue();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            HtmlDocument htmlDom = new HtmlDocument();
+            htmlDom.LoadHtml(responseBody);
+
+            var document = htmlDom.DocumentNode;
+
+            document.QuerySelectorAll("table.persons").Should().NotBeNull();
+
         }
     }
 }
